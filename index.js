793331697ef4337d39ce1bb9d85d0c0ce6b631d5
kmriazuddin <mohammadriazuddin3@gmail.com>
2023-11-08 22:43:54 +0600

    m

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['https://home-serv.netlify.app'],
   credentials: true
 }));
 app.use(express.json());
